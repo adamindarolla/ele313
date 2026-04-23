204cd6b5a9f8df93882f3770fd04eb58c145e1e2
Adam Collen <adamccollen@gmail.com>
2026-04-23 12:09:39 +0100

    Update main.c
    Re-do

--- Project/main.c
+++ Project/main.c
@@ -1,8 +1,8 @@
+// Adam was actually here 22/4/26
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
 #include "ch.h"
 #include "hal.h"
 #include "memory_protection.h"
@@ -11,15 +11,21 @@
 #include "spi_comm.h"
 #include "sensors/proximity.h"
 #include "motors.h"
+#include "epuck1x/uart/e_uart_char.h"
+#include "stdio.h"
+#include "serial_comm.h"
+#include "selector.h"
 
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
 
+// Space to declare wall following function
 
-int main(void)
-{
+// Space for some other stuff
 
+// Initialising stuff
+int main(void){
     halInit();
     chSysInit();
     mpu_init();
@@ -32,31 +38,143 @@ int main(void)
     //LED
     clear_leds();
     spi_comm_start();
+	rgb_led_name_t allRGBs[] = {LED2, LED4, LED6, LED8};
 
     //Motors
     motors_init();
 
-    /* Infinite loop. */
-    while (1)
-    {
-    	if (get_calibrated_prox(0)>=200 || get_calibrated_prox(7)>=200)
-    	{
-    		set_front_led(2);
-    		left_motor_set_speed(500);
-    		right_motor_set_speed(-500);
-    	}
-    	else
-    	{
-    		left_motor_set_speed(500);
-    		right_motor_set_speed(500);
-    	}
+	//Bluetooth
+	serial_start();
+
+    // Declare our stuff
+    int SensorValue[8];
+    int currentVal = 0;
+    int strongestSensor =-1; // put it as -1 to avoid confusing code - will be from 0 to 7 later
+    int maxVal = 0;
+    int maxValThreshold = 500; // for collision detection
+
+    int followSide =0; // will be -1 or +1 later
+    
+
+    int maxirread = 1000;
+    int minirread = 300;
+
+    bool wallsexplored = 0; // flicks to 1 (true) after doing a lap around the walls
+
+    enum RobotState { NOWALL, FOUNDWALL, EXPLORING };
+	enum RobotState currentState = NOWALL;
+
+
+
+    // Main Loop!
+    while(1){
+        //Need some logic that checks immediate surroundings
+        //AND takes into account previous exploration
+        //To decide what currentMode is
+
+            // Check all sensors and get values
+            for (int i = 0, i < 8 , i++){
+
+                int SensorValue(i) = get_prox(i); // Start at 0 and end at 7 for corresponding sensors
+                int currentVal = get_prox(i);
+
+                if (currentVal > maxVal){
+                    maxVal = currentVal;
+                    strongestSensor = i;
+                }
+            }
+                        
+        }
+
+        //Different behaviour based on which state we decided
+
+        if(currentState == NOWALL){
+             if(maxVal >= maxValThreshold){ 
+            switch (strongestSensor) {
+                case 0
+                case 1
+                case 2
+                case 3  
+                    followSide = 1; //right
+                    currentState = FOUNDWALL;
+                case 4
+                case 5
+                case 6
+                case 7
+                    followSide = -1; //left
+                    currentState = FOUNDWALL;
+            }
+            else{
+                left_motor_set_speed(500);
+                right_motor_set_speed(500);
+            }
+        }
+
+        if (currentState == FOUNDWALL){
+            if (wallsexplored = 0){
+                //DO THE WALL FOLLOWING CODE
+
+
+
+
+
+
+                wallsexplored = 1; // ensures we do not re-enter this state - needs a thing that only does it after a certain amount of time
+            }
+            else{
+
+                left_motor_set_speed(500*followSide);
+                right_motor_set_speed(500*followSide);
+                delay_ms(500); // rotate for a certain amount of time
+                left_motor_set_speed(0);
+                right_motor_set_speed(0)
+                currentState = EXPLORING; 
+
+                // This is if we do not want to re-enter wall follow mode i.e. bounce right off the wall
+            }
+        }
+        
+            
+        
+        
+        if(currentState == EXPLORING){
+            // we already know what the sensor values are so no need to get_prox again
+                        
+            if (maxVal > maxValThreshold){
+                switch (strongestSensor) {
+                case 0
+                case 1
+                case 2
+                    followSide = 1; //right
+                    currentState = FOUNDWALL;
+                    
+                case 3  
+                case 4
+                    followside = 0; // ignore these two
+                    currentState = EXPLORING;
+                    left_motor_set_speed(500);
+                    right_motor_set_speed(500);
+                case 5
+                case 6
+                case 7
+                    followSide = -1; //left
+                    currentState = FOUNDWALL;
+                }
+
+            else{
+                left_motor_set_speed(500);
+                right_motor_set_speed(500);
+
+                }
+            }
+            
+        }   
+        
+
+
+
+
+
+        // the purple } is for the while(1) loop leave that!
     }
-}
-
-#define STACK_CHK_GUARD 0xe2dee396
-uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
-
-void __stack_chk_fail(void)
-{
-    chSysHalt("Stack smashing detected");
 }
